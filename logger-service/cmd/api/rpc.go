7714180e10290ddc206bffeb8ce38cdf2cb13ea5
DouglasVolcato <douglasvolcato@gmail.com>
2025-08-03 23:50:01 -0300

    .

--- logger-service/cmd/api/rpc.go
+++ logger-service/cmd/api/rpc.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"log-service/data"
+	"net"
+	"net/rpc"
 	"time"
 )
 
@@ -30,4 +33,22 @@ func (r *RpcServer) LogInfo(payload RpcPayload, resp *string) error {
 
 	*resp = "Processed payload via RPC: " + payload.Name
 	return nil
+}
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+
+		go rpc.ServeConn(rpcConn)
+	}
 }
